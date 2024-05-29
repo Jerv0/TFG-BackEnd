ec0754eb178fce73dccdc3f319b82668b50d0568
Javier Ruiz Martinez <jruizm@contesta.es>
2024-05-29 16:31:09 +0200

    quito mis pruebas y dejo el codigo normal

--- prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli('127.0.0.1', 'root', 'Root1.', 'tfg');
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-echo "Conexión exitosa";
-$conn->close();
-
